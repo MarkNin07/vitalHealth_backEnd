9ca4e7fe0c0891db4ce03a9df9391e5b08e55483
AlbertDeHoz <alberto.entreprises@gmail.com>
2022-07-03 15:16:16 -0500

    ADD:mongo-db connection

--- server.py
+++ server.py
@@ -1,7 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_pymongo import PyMongo
+
+USER="admin"
+PASSWORD="vital"
+DB_NAME="vitalHealthDB"
+MONGO_URI = f"mongodb+srv://{USER}:{PASSWORD}@cluster0.8kl8u.mongodb.net/{DB_NAME}?retryWrites=true&w=majority"
 
 app = Flask(__name__)
+mongo = PyMongo(app, uri=MONGO_URI)
+
+
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/products")
+def get_products():
+    products = mongo.db.products.find({})
+    product_list = [to_json(product) for product in products]
+    return jsonify(product_list)
+    
+def to_json(product):
+    product["_id"] = str(product["_id"])
+    return product
